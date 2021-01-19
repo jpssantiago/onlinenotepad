e58363444bf6a7923ac600cb14a3baf6ef0e0e8b
jpssantiago <jps_santiago@outlook.com>
2021-01-19 17:23:14 -0300

    First commit.

---
+++ src/components/Container/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+function Container() {
+    return (
+        <div className="container">
+            <div className="container-controls">
+
+            </div>
+            
+            <div className="container-textarea">
+
+            </div>
+        </div>
+    );
+}
+
+export default Container;
